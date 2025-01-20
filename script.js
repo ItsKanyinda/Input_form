f2173621dd5911478c66fcd5467b1f6da67852fc
Michael kanyinda <77644685+ItsKanyinda@users.noreply.github.com>
2025-01-20 14:01:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+
 const SUPABASE_URL = 'https://nggucsasyksohvjnjzru.supabase.co'; // Replace with your Supabase URL
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nZ3Vjc2FzeWtzb2h2am5qenJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2ODU5NzYsImV4cCI6MjA1MjI2MTk3Nn0.AaogHxq1WAJPHyrn8t4fZ9I_VjXchAAUP5bQVEqwcSI'; // Replace with your Supabase anon key
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
